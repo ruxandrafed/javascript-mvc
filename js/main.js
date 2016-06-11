ab82dda5605e43256bc693615ec2bb60a25bb507
ruxandrafed <ruxandra.fediuc@gmail.com>
2016-06-11 00:04:36 -0700

    Add require.js for JS module loading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-  for (ii=1; ii<=24; ii++) {
-    $('.modal-trigger-' + ii).leanModal();
+requirejs.config({
+  paths: {
+    'jquery': 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
+    'materialize': 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min'
+  },
+  "shim": {
+    "materialize": ["jquery"]
   }
 });
+
+require(["jquery", "materialize"],
+
+  function($) {
+    $(document).ready(function(){
+
+      // modals initialization
+      for (ii=1; ii<=24; ii++) {
+        $('.modal-trigger-' + ii).leanModal();
+      }
+
+    });
+  }
+
+);
