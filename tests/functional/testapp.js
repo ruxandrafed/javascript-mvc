2fcf8e1e0c72c43797ac7825c3fa1623001f3fd4
ruxandrafed <ruxandra.fediuc@gmail.com>
2016-06-12 22:23:13 -0700

    Trying casperjs

---
+++ tests/functional/testapp.js
@@ -0,0 +1,25 @@
+// var casper = require('casper').create();
+var fs = require('fs');
+var filePath = 'file:///' + fs.workingDirectory + '/index.html';
+
+casper.test.begin('Page title', 2, function suite(test) {
+  casper.start(filePath, function() {
+    test.assertExists('title');
+    test.assertSelectorHasText('title', 'Best Buy MVC');
+  }).run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin('Category list', 1, function suite(test) {
+  casper.start(filePath, function() {
+    test.assertExists('.category-filter');
+  }).run(function() {
+    test.done();
+  });
+});
+
+
+// this.wait(5000, function() {
+//   console.log(this.getHTML());
+// });
